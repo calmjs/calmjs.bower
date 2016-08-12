98071f1f932450d0b1a2fcc187b9d45fb5a18fa5
Tommy Yu <tommy.yu@auckland.ac.nz>
2016-08-12 22:57:32 +1200

    Fix the test_suite.

--- src/calmjs/bower/tests/__init__.py
+++ src/calmjs/bower/tests/__init__.py
@@ -2,13 +2,10 @@ import unittest
 from os.path import dirname
 
 
-def make_suite():
-    import calmjs.bower
+def make_suite():  # pragma: no cover
     test_loader = unittest.TestLoader()
     test_suite = test_loader.discover(
         'calmjs.bower.tests', pattern='test_*.py',
-        # namespace packages are actually going to interfere if not
-        # very explicit here.
-        top_level_dir=dirname(calmjs.bower.__file__)
+        top_level_dir=dirname(__file__)
     )
     return test_suite
